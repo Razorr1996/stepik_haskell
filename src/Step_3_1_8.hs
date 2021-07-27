de6c18ec968b21724a8202164d13a8a8bed360c2
Artem Basalaev <basa62.1996@gmail.com>
2021-07-27 11:46:15 +0300

    Step_3_1_8.hs

---
+++ src/Step_3_1_8.hs
@@ -0,0 +1,15 @@
+module Step_3_1_8 where
+
+import Control.Monad.Trans.Except
+
+data ReadError = EmptyInput | NoParse String
+  deriving (Show, Eq)
+
+-- region Task8
+tryRead :: Read a => String -> Except ReadError a
+tryRead "" = throwE EmptyInput
+tryRead s = do
+  case reads s of
+    [(x, "")] -> return x
+    _ -> throwE $ NoParse s
+-- endregion
