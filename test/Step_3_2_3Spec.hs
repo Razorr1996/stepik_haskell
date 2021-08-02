168145f5e55cc425f05d96c6ed0482e4d585516b
Artem Basalaev <basa62.1996@gmail.com>
2021-08-02 17:17:16 +0300

    Indents in Step_3_2_3Spec.hs

--- test/Step_3_2_3Spec.hs
+++ test/Step_3_2_3Spec.hs
@@ -6,30 +6,23 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "CPS" $ do
-    it "decode one as a number" $
-      do
-        decode one as a number `shouldBe` (1 :: Integer)
+    it "decode one as a number" $ do
+      decode one as a number `shouldBe` (1 :: Integer)
 
-    it "decode one hundred twenty three as a number" $
-      do
-        decode one hundred twenty three as a number `shouldBe` (123 :: Integer)
+    it "decode one hundred twenty three as a number" $ do
+      decode one hundred twenty three as a number `shouldBe` (123 :: Integer)
 
-    it "decode one hundred twenty one as a number" $
-      do
-        decode one hundred twenty one as a number `shouldBe` (121 :: Integer)
+    it "decode one hundred twenty one as a number" $ do
+      decode one hundred twenty one as a number `shouldBe` (121 :: Integer)
 
-    it "decode one hundred twenty as a number" $
-      do
-        decode one hundred twenty as a number `shouldBe` (120 :: Integer)
+    it "decode one hundred twenty as a number" $ do
+      decode one hundred twenty as a number `shouldBe` (120 :: Integer)
 
-    it "decode one hundred as a number" $
-      do
-        decode one hundred as a number `shouldBe` (100 :: Integer)
+    it "decode one hundred as a number" $ do
+      decode one hundred as a number `shouldBe` (100 :: Integer)
 
-    it "decode three hundred as a number" $
-      do
-        decode three hundred as a number `shouldBe` (300 :: Integer)
+    it "decode three hundred as a number" $ do
+      decode three hundred as a number `shouldBe` (300 :: Integer)
 
-    it "decode two thousand seventeen as a number" $
-      do
-        decode two thousand seventeen as a number `shouldBe` (2017 :: Integer)
+    it "decode two thousand seventeen as a number" $ do
+      decode two thousand seventeen as a number `shouldBe` (2017 :: Integer)
