8dc517a7169fff9f684354ea3d2fadc8f00f56a6
Artem Basalaev <basa62.1996@gmail.com>
2021-08-05 20:44:39 +0300

    Step_3_3_4.hs

---
+++ test/Step_3_3_4Spec.hs
@@ -0,0 +1,17 @@
+module Step_3_3_4Spec where
+
+import Step_3_3_4
+
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Writer
+
+import Test.Hspec
+
+strings :: [String]
+strings = ["abc", "defg", "hij"]
+
+spec :: Spec
+spec = do
+  describe "logFirstAndRetSecond" $ do
+    it "runReader (runWriterT logFirstAndRetSecond) strings" $ do
+      runReader (runWriterT logFirstAndRetSecond) strings `shouldBe` ("DEFG", "abc")
