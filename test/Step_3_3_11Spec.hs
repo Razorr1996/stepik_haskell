826aa489302d09f1de09475a3fa84ba4413b2105
Artem Basalaev <basa62.1996@gmail.com>
2021-08-09 22:48:45 +0300

    Step_3_3_11.hs

---
+++ test/Step_3_3_11Spec.hs
@@ -0,0 +1,28 @@
+module Step_3_3_11Spec where
+
+import Control.Monad
+import Step_3_3_11
+import Test.Hspec
+
+spec :: Spec
+spec = parallel $ do
+  describe "EsSi" $ do
+    it "runEsSi (go 1 85 tickCollatz) 27" $
+      do
+        runEsSi (go 1 85 tickCollatz) 27
+        `shouldBe` ((Right (), 82) :: (Either String (), Integer))
+
+    it "runEsSi (go 1 80 tickCollatz) 27" $
+      do
+        runEsSi (go 1 80 tickCollatz) 27
+        `shouldBe` ((Left "Upper bound", 82) :: (Either String (), Integer))
+
+    it "runEsSi (forever $ go 1 1000 tickCollatz) 27" $
+      do
+        runEsSi (forever $ go 1 1000 tickCollatz) 27
+        `shouldBe` ((Left "Upper bound", 1186) :: (Either String (), Integer))
+
+    it "runEsSi (forever $ go 1 10000 tickCollatz) 27" $
+      do
+        runEsSi (forever $ go 1 10000 tickCollatz) 27
+        `shouldBe` ((Left "Lower bound", 1) :: (Either String (), Integer))
