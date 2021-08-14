f18ec1cb32ed1fef3466be52ba4b0b79a3adf172
Artem Basalaev <basa62.1996@gmail.com>
2021-08-14 13:58:10 +0300

    Step_3_5_7.hs

---
+++ src/Step_3_5_7.hs
@@ -0,0 +1,34 @@
+module Step_3_5_7 where
+
+import Control.Applicative (liftA2)
+
+data Logged a = Logged String a deriving (Eq, Show)
+
+newtype LoggT m a = LoggT {runLoggT :: m (Logged a)}
+
+-- region Task
+-- code
+instance Functor m => Functor (LoggT m) where
+  fmap f = LoggT . fmap updater . runLoggT
+    where
+      updater (Logged l a) = Logged l (f a)
+
+instance Applicative m => Applicative (LoggT m) where
+  pure x = LoggT $ pure $ Logged mempty x
+
+  f <*> v = LoggT $ liftA2 updater (runLoggT f) (runLoggT v)
+    where
+      updater (Logged l1 f') (Logged l2 v') = Logged (l1 `mappend` l2) (f' v')
+
+instance Monad m => Monad (LoggT m) where
+  return = pure
+
+  m >>= k = LoggT $ do
+    ~(Logged l1 v) <- runLoggT m
+    ~(Logged l2 v') <- runLoggT (k v)
+    return $ Logged (l1 `mappend` l2) v'
+
+instance MonadFail m => MonadFail (LoggT m) where
+  fail = LoggT . fail
+
+-- endregion
---
+++ test/Step_3_5_7Spec.hs
@@ -0,0 +1,41 @@
+module Step_3_5_7Spec where
+
+import Control.Monad.Identity
+import Step_3_5_7
+import Test.Hspec
+
+logTst :: LoggT Identity Integer
+logTst = do
+  x <- LoggT $ Identity $ Logged "AAA" 30
+  y <- return 10
+  z <- LoggT $ Identity $ Logged "BBB" 2
+  return $ x + y + z
+
+failTst :: [Integer] -> LoggT [] Integer
+failTst xs = do
+  5 <- LoggT $ fmap (Logged "") xs
+  LoggT [Logged "A" ()]
+  return 42
+
+spec :: Spec
+spec = parallel $ do
+  describe "LoggT: Functor, Applicative, Monad" $ do
+    it "runIdentity (runLoggT logTst)" $
+      do
+        runIdentity (runLoggT logTst)
+        `shouldBe` Logged "AAABBB" 42
+
+    it "runLoggT $ failTst [5,5]" $
+      do
+        runLoggT $ failTst [5, 5]
+        `shouldBe` [Logged "A" 42, Logged "A" 42]
+
+    it "runLoggT $ failTst [5,6]" $
+      do
+        runLoggT $ failTst [5, 6]
+        `shouldBe` [Logged "A" 42]
+
+    it "runLoggT $ failTst [7,6]" $
+      do
+        runLoggT $ failTst [7, 6]
+        `shouldBe` []
