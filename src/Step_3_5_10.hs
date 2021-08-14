1b685b39eefa1dbe8f1e19e5a35077be816d3c70
Artem Basalaev <basa62.1996@gmail.com>
2021-08-14 14:12:49 +0300

    Step_3_5_10.hs

---
+++ src/Step_3_5_10.hs
@@ -0,0 +1,16 @@
+module Step_3_5_10 where
+
+import Control.Monad.Identity
+import Step_3_5_7
+
+-- region Task
+-- code
+write2log :: Monad m => String -> LoggT m ()
+write2log s = LoggT $ return $ Logged s ()
+
+type Logg = LoggT Identity
+
+runLogg :: Logg a -> Logged a
+runLogg = runIdentity . runLoggT
+
+-- endregion
---
+++ test/Step_3_5_10Spec.hs
@@ -0,0 +1,33 @@
+module Step_3_5_10Spec where
+
+import Control.Monad.State
+import Step_3_5_10
+import Step_3_5_7
+import Test.Hspec
+
+logTst' :: Logg Integer
+logTst' = do
+  write2log "AAA"
+  write2log "BBB"
+  return 42
+
+stLog :: StateT Integer Logg Integer
+stLog = do
+  modify (+ 1)
+  a <- get
+  lift $ write2log $ show $ a * 10
+  put 42
+  return $ a * 100
+
+spec :: Spec
+spec = parallel $ do
+  describe "LoggT: write2log" $ do
+    it "runLogg logTst'" $
+      do
+        runLogg logTst'
+        `shouldBe` Logged "AAABBB" 42
+
+    it "runLogg $ runStateT stLog 2" $
+      do
+        runLogg $ runStateT stLog 2
+        `shouldBe` Logged "30" (300, 42)
