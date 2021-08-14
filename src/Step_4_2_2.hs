d9f3ff610a1217975cd68ea52414acad4bba8006
Artem Basalaev <basa62.1996@gmail.com>
2021-08-15 00:42:02 +0300

    Step_4_2_2.hs

---
+++ src/Step_4_2_2.hs
@@ -0,0 +1,17 @@
+module Step_4_2_2 where
+
+import Control.Monad.State (StateT (..))
+
+-- region Task
+-- code
+evalStateT :: Monad m => StateT s m a -> s -> m a
+evalStateT m s = do
+  ~(a, _) <- runStateT m s
+  return a
+
+execStateT :: Monad m => StateT s m a -> s -> m s
+execStateT m s = do
+  ~(_, s') <- runStateT m s
+  return s'
+
+-- endregion
